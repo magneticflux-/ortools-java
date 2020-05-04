dd1d11b5a6d1738a513f8bf53c2c89572474d309
Mitchell Skaggs <skaggsm333@gmail.com>
2020-05-03 20:08:14 -0500

    Add Kotlin and JVM overloads for a Class and ClassLoader parameter

--- ortools-loader/src/main/kotlin/com/skaggsm/ortools/OrToolsHelper.kt
+++ ortools-loader/src/main/kotlin/com/skaggsm/ortools/OrToolsHelper.kt
@@ -8,15 +8,25 @@ import com.sun.jna.Platform
  * Helper to set up and load OR-Tools.
  */
 object OrToolsHelper {
+
     /**
      * Call before using Google OR-Tools.
      */
     @JvmStatic
-    fun loadLibrary() {
+    @JvmOverloads
+    fun loadLibrary(clazz: Class<*> = this.javaClass) {
+        return loadLibrary(clazz.classLoader)
+    }
+
+    /**
+     * Call before using Google OR-Tools.
+     */
+    @JvmStatic
+    fun loadLibrary(loader: ClassLoader) {
         val path = ClasspathUtils.extractResourcesToTempDirectory(
             "${Platform.RESOURCE_PREFIX}/",
             "ortools-java",
-            this.javaClass
+            loader
         )
         System.load(path.resolve(Platform.RESOURCE_PREFIX).resolve(System.mapLibraryName("jniortools")).toString())
     }
