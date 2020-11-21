971c0a7cfe76b53125d3f9d9f505149ff62efaa2
Mitchell Skaggs <skaggsm333@gmail.com>
2020-11-21 12:57:56 -0600

    Update all dependencies for 8.0

--- ortools-loader/build.gradle.kts
+++ ortools-loader/build.gradle.kts
@@ -3,22 +3,22 @@ import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
     `java-library`
-    kotlin("jvm") version "1.4.0"
-    id("com.github.ben-manes.versions") version "0.30.0"
+    kotlin("jvm") version "1.4.20"
+    id("com.github.ben-manes.versions") version "0.36.0"
 }
 
 val bundle: Configuration by configurations.creating
 
 dependencies {
-    implementation("com.google.protobuf:protobuf-java:3.12.2")
+    implementation("com.google.protobuf:protobuf-java:3.13.0")
     implementation("com.skaggsm:classpath-resource-extractor:0.2.0")
     api(files("libs/com.google.ortools.jar"))
     bundle(files("libs/com.google.ortools.jar"))
 
     testRuntimeOnly(project(":ortools-natives-all"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0-RC1")
-    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.2.3")
-    testImplementation("io.kotest:kotest-assertions-core-jvm:4.2.3")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.7.0")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:4.3.1")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:4.3.1")
     testImplementation("org.slf4j:slf4j-nop:2.0.0-alpha1")
 }
 
