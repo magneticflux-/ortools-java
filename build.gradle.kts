bec70cfb90cc9b664a8ec8b69de591c7ae67040f
Mitchell Skaggs <skaggsm333@gmail.com>
2021-10-31 00:21:43 -0500

    Update dependencies, migrate to official publications

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,16 @@
+import com.github.benmanes.gradle.versions.updates.DependencyUpdatesTask
+
 plugins {
     id("org.shipkit.java") version "2.3.5"
-    id("com.github.ben-manes.versions") version "0.36.0"
+    id("com.github.ben-manes.versions") version "0.39.0"
+}
+
+tasks.withType<DependencyUpdatesTask> {
+    gradleReleaseChannel = "current"
+    rejectVersionIf {
+        candidate.version.contains("""[-.]M\d+""".toRegex()) ||
+                candidate.version.contains("RC")
+    }
 }
 
 subprojects {
@@ -9,8 +19,7 @@ subprojects {
     group = "com.skaggsm.ortools"
 
     repositories {
-        jcenter()
-        maven("https://dl.bintray.com/magneticflux/maven")
+        mavenCentral()
     }
 
     configure<JavaPluginConvention> {
--- ortools-natives-linux/build.gradle.kts
+++ ortools-natives-linux/build.gradle.kts
@@ -4,4 +4,5 @@ plugins {
 
 dependencies {
     api(project(":ortools-loader"))
+    api("com.google.ortools:ortools-linux-x86-64:9.1.9491")
 }
